b82dbc1f9e8e97df44a8bd30aa4d225ad97258b5
vladRak <vladrakitianskiy@gmail.com>
2019-11-19 22:34:12 +0100

    First Commit

---
+++ hw1/lecture_3.js
@@ -0,0 +1,37 @@
+var array = [
+	{
+		name: 'Dmytro',
+	  mark: 3
+	},
+	{
+		name: 'Denis',
+	  mark: 4
+	 },
+	{
+		name: 'Alex',
+		mark: 5
+	}
+];
+// NB!
+// Window
+// HTMLDocument
+// API and methods
+function getMarks(item) {
+		var result;
+		if(item.mark > 3){
+			result = `${item.name}:  ${item.mark}`;
+		}else {
+			result = `${item.name} has bad mark!`;
+		}
+		return result;
+};
+
+array.forEach(function printMarks(item) {
+		console.log(getMarks(item));
+});
+
+// for(var i in array){
+// 	if(array[i].mark > 3){
+// 		console.log(array[i].name + ': ' + array[i].mark);
+// 	}
+// }
