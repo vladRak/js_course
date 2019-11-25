c27ed0e83a5869afcf206e28b840bb9a0932e4c5
vladRak <vladrakitianskiy@gmail.com>
2019-11-25 11:46:25 +0100

    hw2.1

--- hw2/task_5.js
+++ hw2/task_5.js
@@ -1,21 +1,22 @@
 // 5. найти наиболее часто встречающийся элемент(-ы) в массиве //[1,2,3,1] -> 1
 
-function mostFrequent(array){
-  var result = array[0];
+function mostFrequent(array) {
+  var result = Array.from(new Set(array));
   var tmp = 0;
-  for(var i of array){
+  for (var i of array) {
     var count = 0;
-    for(var j of array){
-      if(i===j){
+    for (var j of array) {
+      if (i === j) {
         count++;
       }
     }
-    if(count > tmp){
+    if (count >= tmp) {
       tmp = count;
-      result = i;
+    } else {
+      result.splice(result.indexOf(i), (result.indexOf(i) != -1 ? 1 : 0))
     }
   }
   return result;
 }
 
-console.log(mostFrequent([3, 'a', 'a', 'a', 2, 3, 'a', 3, 'a', 2, 4, 9, 3]));
+console.log(mostFrequent([3, 'a', 'a', 'a', 2, 3, 'a', 3, 'a', 2, 3, 4, 9, 3]));
